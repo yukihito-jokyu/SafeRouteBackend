c350e02555cc678a7fe1df12f387bead07c89c2b
yukihito-jokyu <119581059+yukihito-jokyu@users.noreply.github.com>
2025-06-09 11:37:54 +0900

    :tada: 初期化

---
+++ src/main/java/com/osc/saferoute/SaferouteApplication.java
@@ -0,0 +1,13 @@
+package com.osc.saferoute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaferouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaferouteApplication.class, args);
+	}
+
+}
